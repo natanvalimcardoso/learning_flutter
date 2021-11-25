2c280a3324170ae0572c1d55b749823897cf4674
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-11-25 16:08:25 -0300

    new

--- lib/controller.dart
+++ lib/controller.dart
@@ -8,28 +8,26 @@ class Controller extends StatefulWidget {
 }
 
 class _ControllerState extends State<Controller> {
-  final _pesoController = TextEditingController();
 
+  final _valorController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: (TextField(
-              controller: _pesoController,
-              keyboardType: TextInputType.number,
-              // ignore: prefer_const_constructors
-              decoration: InputDecoration(
-                labelText: 'Peso',
-                hintText: "Digite o seu peso",
-                border: OutlineInputBorder(),
-              ))),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Teste'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              _valorController.clear();
+            },
+          )
+        ],
       ),
+    body: const SingleChildScrollView()
     );
   }
 }
