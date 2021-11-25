e55be7eb0bcb00bcf4a86fb4ae140150d0d0a477
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-11-25 16:44:52 -0300

    controller

--- lib/controller.dart
+++ lib/controller.dart
@@ -8,26 +8,43 @@ class Controller extends StatefulWidget {
 }
 
 class _ControllerState extends State<Controller> {
-
   final _valorController = TextEditingController();
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Teste'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () {
-              _valorController.clear();
-            },
-          )
-        ],
-      ),
-    body: const SingleChildScrollView()
-    );
+        appBar: AppBar(
+          title: const Text('Teste'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                _valorController.clear();
+              },
+            )
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(children: <Widget>[
+            TextField(
+              controller: _valorController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Digite um valor',
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_valorController.text.isNotEmpty) {
+                    final int valor = int.parse(_valorController.text);
+                    print(valor); //Serviu para mostrar no console de debug
+                  }
+                },
+                child: const Text('Enviar'))
+          ]),
+        ));
   }
 }
