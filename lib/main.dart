37bb77c92eb81a44907efd4e8ba3e65f2383f81c
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-02-09 13:16:07 -0300

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/widgets/Listview_builder.dart';
 import 'package:learn_flutter/widgets/controller.dart';
+import 'package:learn_flutter/widgets/flexible.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:   Controller(),
+      home:  FlexibleWidget(),
       debugShowCheckedModeBanner: false,
       
     );
