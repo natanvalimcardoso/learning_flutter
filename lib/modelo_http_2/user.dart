336517d52c623046123545aa9d28f76bc00f5712
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-07-27 19:06:22 -0300

    ready

--- lib/modelo_http_2/user.dart
+++ lib/modelo_http_2/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'user_types.dart';
 
 class User {
@@ -13,13 +15,35 @@ class User {
     required this.userTypes,
   });
 
+//---------------------//--------------------//
+
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
         id: map['id'] ?? '',
         name: map['name'] ?? '',
         userName: map['username'] ?? '',
-        userTypes: map['user_types']?.map());
+        userTypes: map['user_types']?.map().toList((type) {
+              return UserTypes.fromMap(type);
+            }) ??
+            []);
   }
+
+//---------------------//--------------------//
+
+  factory User.fromJson(String source) => User.fromMap(jsonDecode(source));
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'username': userName,
+      'userTypes': userTypes.map((e) => e.toMap()).toList()
+    };
+  }
+
+//---------------------//--------------------//
+
+  String toJson() => jsonEncode(toMap());
 }
 
 
--- lib/modelo_http_2/user_types.dart
+++ lib/modelo_http_2/user_types.dart
@@ -1,3 +1,41 @@
-class UserTypes{
-  
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserTypes {
+  final String name;
+  final String id;
+  final String userId;
+
+  UserTypes({
+    required this.id,
+    required this.userId,
+    required this.name,
+  });
+
+//---------------------//--------------------//
+
+  factory UserTypes.fromMap(Map<String, dynamic> map) {
+    return UserTypes(
+        id: map['id'] ?? '',
+        userId: map['userId'] ?? '',
+        name: map['name'] ?? '');
+  }
+
+//---------------------//--------------------//
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'userId': userId,
+    };
+  }
+
+  //---------------------//--------------------//
+
+  factory UserTypes.fromJson(String source) => UserTypes.fromMap(jsonDecode(source));
+
+  //---------------------//--------------------//
+
+  String toJson() => jsonEncode(toMap());
 }
