7b4cce4abc844307ca1a53dc53ea483ac27e0314
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-07-29 15:01:21 -0300

    ready

--- lib/streams/periodic.dart
+++ lib/streams/periodic.dart
@@ -1,4 +1,12 @@
-void main() {
-  final interval;
-  Stream.periodic()
+Future<void> main() async {
+  final interval = Duration(seconds: 2);
+  final stream = Stream<int>.periodic(interval, callback);
+
+  await for (var element in stream) {
+    print(element);
+  }
+}
+
+int callback(int value) {
+  return (value + 1) * 2;
 }
