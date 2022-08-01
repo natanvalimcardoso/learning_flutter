1f8b1e41bb7904ce8c94321f51ee2ce5996da697
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-08-01 10:12:55 -0300

    ready

--- lib/api_dart/lib/model/telefone.dart
+++ lib/api_dart/lib/model/telefone.dart
@@ -1,9 +1,39 @@
+import 'dart:convert';
+
 class Telefone {
   int ddd;
   String numero;
 
   Telefone({required this.ddd, required this.numero});
+
+//* toMap = É um método que pega o objeto (Telefone) e transforma em um Map<String, dynamic>
+  Map<String, dynamic> toMap() {
+    return {
+      'ddd': ddd,
+      'numero': numero,
+    };
+  }
+
+//* fromMap  = É um construtor, ele vai pegar um Map e transformar em um objeto (Telefone)
+  factory Telefone.fromMap(Map<String, dynamic> map) {
+    return Telefone(
+      ddd: map['ddd'] ?? 0,
+      numero: map['numero'] ?? '',
+    );
+  }
+
+//* toJson = É um método que retorna uma string json do meu objeto (Telefone)
+  String toJson() => jsonEncode(toMap());
+
+//* fromJson = É um método baseado em uma string json que retorna um objeto
+  factory Telefone.fromJson(String json) => Telefone.fromMap(jsonDecode(json));
 }
+
+
+
+
+
+
 
 // telefone: {
 // ddd: 11,
