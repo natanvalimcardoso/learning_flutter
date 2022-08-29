0e058dd9b360a439576448b02719ea3d320f4c5f
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-08-29 14:28:16 -0300

    ready

--- lib/academia/circle_avatar/circle_avatar_page.dart
+++ lib/academia/circle_avatar/circle_avatar_page.dart
@@ -9,28 +9,71 @@ class CircleAvatarPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Circle Avatar'),
       ),
-      body: Container(
-        width: 100,
-        height: 100,
-        child: const CircleAvatar(
-          backgroundImage: NetworkImage(
-            'https://upload.wikimedia.org/wikipedia/commons/8/83/Bra-Cos_%281%29_%28cropped%29.jpg',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          ImageAvatar(
+            urlImage:
+                'https://upload.wikimedia.org/wikipedia/commons/8/83/Bra-Cos_%281%29_%28cropped%29.jpg',
           ),
-        ),
+          ImageAvatar(
+            urlImage:
+                'https://www.perfildosfamosos.com/megan-fox/foto-perfil-megan-fox.jpg?v=e86f06a48cf4b7aa94210f594dc621a0',
+          ),
+        ],
       ),
     );
   }
 }
 
-
 class ImageAvatar extends StatelessWidget {
   final String urlImage;
-  const ImageAvatar({ Key? key, required this.urlImage }) : super(key: key);
+  const ImageAvatar({Key? key, required this.urlImage}) : super(key: key);
 
-   @override
-   Widget build(BuildContext context) {
-       return Container(
-           
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(100)),
+        ),
+        SizedBox(
+          height: 100,
+          width: 100,
+          child: Padding(
+            padding: const EdgeInsets.all(4),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                urlImage,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 100,
+          width: 100,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(5)
+              ),
+              child: const Text(
+                'AO VIVO',
+                style: TextStyle(
+                  fontSize: 8,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
