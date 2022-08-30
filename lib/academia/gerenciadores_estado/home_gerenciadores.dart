3ddfa4ba67e0290bc843e09fc423c3633332f54e
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-08-30 09:55:02 -0300

    ready

--- lib/academia/gerenciadores_estado/home_gerenciadores.dart
+++ lib/academia/gerenciadores_estado/home_gerenciadores.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
-    
-class HomeGerenciadores extends StatelessWidget {
 
-  const HomeGerenciadores({ Key? key }) : super(key: key);
-  
+class HomeGerenciadores extends StatelessWidget {
+  const HomeGerenciadores({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('I.M.C'),
       ),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('SetState'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('ValueNotifier'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('ChangeNotifier'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Bloc Patterns (Streams)'),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/academia/gerenciadores_estado/setState/imc_setState.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImcSetState extends StatelessWidget {
+
+  const ImcSetState({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           
+           body: Container(),
+       );
+  }
+}
